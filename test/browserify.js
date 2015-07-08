ab432728e0dfa9f7342ce74fbd90a9f21f36d511
Jasper van de Gronde <th.v.d.gronde@hccnet.nl>
2015-07-08 15:09:10 +0200

    Fixed browserify test.

--- test/browserify.js
+++ test/browserify.js
@@ -6,16 +6,31 @@ var path = require("path")
 var tape = require("tape")
 
 var cases = [ "unary", "binary", "offset" ]
+var srcs = new Array(cases.length)
+var srcsBundled = 0
 
-for(var i=0; i<cases.length; ++i) {
+bundleCasesFrom(0)
+
+function bundleCasesFrom(i) {
+  if (i>=cases.length) return
   var b = browserify()
+  b.ignore("tape")
   b.add(__dirname + "/" + cases[i] + ".js")
   b.transform(path.normalize(__dirname + "/../cwise.js"))
   b.bundle(function(err, src) {
     if(err) {
       throw new Error("failed to bundle!")
     }
-    vm.runInNewContext(src, { 
+    srcs[i] = src
+    srcsBundled++
+    if (srcsBundled === cases.length) runTests()
+  })
+  bundleCasesFrom(i+1)
+}
+
+function runTests() { // Apparently tape does not like to be called asynchronously, so we just wait until everything is bundled, and then run all the tests.
+  for(var i=0; i<srcs.length; i++) {
+      vm.runInNewContext(srcs[i], { 
       test: tape,
       Buffer: Buffer,
       Int8Array: Int8Array,
@@ -28,5 +43,5 @@ for(var i=0; i<cases.length; ++i) {
       Uint32Array: Uint32Array,
       Uint8ClampedArray: Uint8ClampedArray,
       console: { log: console.log.bind(console) } })
-  })
+  }
 }
