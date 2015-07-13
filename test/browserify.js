1a00a6b84ef193ed6412a2d8246f241c0c5c829a
Jasper van de Gronde <th.v.d.gronde@hccnet.nl>
2015-07-13 13:39:26 +0200

    Let tape work with the browserify tests.

--- test/browserify.js
+++ test/browserify.js
@@ -6,8 +6,6 @@ var path = require("path")
 var tape = require("tape")
 
 var cases = [ "unary", "binary", "offset" ]
-var srcs = new Array(cases.length)
-var srcsBundled = 0
 
 bundleCasesFrom(0)
 
@@ -17,31 +15,27 @@ function bundleCasesFrom(i) {
   b.ignore("tape")
   b.add(__dirname + "/" + cases[i] + ".js")
   b.transform(path.normalize(__dirname + "/../cwise.js"))
-  b.bundle(function(err, src) {
-    if(err) {
-      throw new Error("failed to bundle!")
-    }
-    srcs[i] = src
-    srcsBundled++
-    if (srcsBundled === cases.length) runTests()
+  tape(cases[i], function(t) { // Without nested tests, the asynchronous nature of bundle causes issues with tape... 
+    b.bundle(function(err, src) {
+      if(err) {
+        throw new Error("failed to bundle!")
+      }
+      vm.runInNewContext(src, { 
+        test: t.test.bind(t),
+        Buffer: Buffer,
+        Int8Array: Int8Array,
+        Int16Array: Int16Array,
+        Int32Array: Int32Array,
+        Float32Array: Float32Array,
+        Float64Array: Float64Array,
+        Uint8Array: Uint8Array,
+        Uint16Array: Uint16Array,
+        Uint32Array: Uint32Array,
+        Uint8ClampedArray: Uint8ClampedArray,
+        console: { log: console.log.bind(console) }
+      })
+      t.end()
+    })
   })
   bundleCasesFrom(i+1)
-}
-
-function runTests() { // Apparently tape does not like to be called asynchronously, so we just wait until everything is bundled, and then run all the tests.
-  for(var i=0; i<srcs.length; i++) {
-      vm.runInNewContext(srcs[i], { 
-      test: tape,
-      Buffer: Buffer,
-      Int8Array: Int8Array,
-      Int16Array: Int16Array,
-      Int32Array: Int32Array,
-      Float32Array: Float32Array,
-      Float64Array: Float64Array,
-      Uint8Array: Uint8Array,
-      Uint16Array: Uint16Array,
-      Uint32Array: Uint32Array,
-      Uint8ClampedArray: Uint8ClampedArray,
-      console: { log: console.log.bind(console) } })
-  }
 }
